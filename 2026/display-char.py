1debf15294058e56b10ba88b71d36e880fb60c3d
z4m9 <samuelmarriott+github@gmail.com>
2026-03-15 11:44:15 +1100

    pf 7.4 - display characters

---
+++ 2026/display-char.py
@@ -0,0 +1,22 @@
+# Display Character
+# Created by Samuel Marriott on 15/03/2026
+
+'''1. Asks the user to input a string.
+2. Asks the user to input an integer (index)
+3. Displays the character at that index in the string. (input_string[index]).'''
+
+while True:
+    try:
+        input_string = input("Say something: ")
+        index = int(input("Enter a number: "))
+        #print(input_string, index)
+        if index < len(input_string):
+            char_at_index = (input_string[index])
+            if char_at_index == " ":
+                print("The character is a space.")
+            print("The character at index", index, "is", char_at_index) # Displays character at index
+            break
+        else:
+            print("Please try again.")
+    except ValueError:
+        print("Please try again.")
