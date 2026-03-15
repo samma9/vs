8b54b45c18417aa06ebb9b70992be3e93ffbd61c
z4m9 <samuelmarriott+github@gmail.com>
2026-03-15 11:52:55 +1100

    Comments tweaked

--- 2026/display-char.py
+++ 2026/display-char.py
@@ -11,8 +11,8 @@ while True:
             if char_at_index == " ":
                 print("The character is an empty space.")
             print("The character at index", index, "is", char_at_index) # Displays character at index
-            break # Inputs are valid: end while loop
+            break # Inputs are valid: End
         else:
-            print("Please try again.")
+            print("Please try again.") # Int number larger than string: Try again
     except ValueError:
-        print("Please try again.") # Loop will continue until inputs are valid
+        print("Please try again.") # Inputs are invalid: Try again
